0c825df582c68c18309fea1adeed1ed8e5949b71
SEIF <seif.kowatli@hotmail.com>
2024-03-05 14:07:53 +0400

    add useWebsocket hook

---
+++ frontend/src/hooks/utils/useWebSocket.ts
@@ -0,0 +1,61 @@
+// useWebSocket.ts
+
+import { useEffect, useState } from 'react';
+import io, { Socket } from 'socket.io-client';
+import { socketUri } from '~/configs';
+
+interface WebSocketHookOptions {
+  eventsToListen?: string[];
+}
+
+interface WebSocketHookResult {
+  sendMessage: (message: string, eventToSend?: string) => void;
+  receivedData: any;
+}
+
+const useWebSocket = ({
+  eventsToListen,
+}: WebSocketHookOptions = {}): WebSocketHookResult => {
+  const url = socketUri;
+
+  const [socket, setSocket] = useState<Socket | null>(null);
+  const [receivedData, setReceivedData] = useState<any>(null);
+
+  useEffect(() => {
+    const newSocket = io(url);
+
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.disconnect();
+    };
+  }, [url]);
+
+  const sendMessage = (message: string, eventToSend: string = 'sendMessage'): void => {
+    if (socket) {
+      socket.emit(eventToSend, { message });
+    }
+  };
+
+  useEffect(() => {
+    if (socket && eventsToListen) {
+      eventsToListen.forEach((event) => {
+        socket.on(event, (data) => {
+          setReceivedData(data);
+        });
+      });
+    }
+
+    return () => {
+      if (socket && eventsToListen) {
+        eventsToListen.forEach((event) => {
+          socket.off(event);
+        });
+      }
+    };
+  }, [socket, eventsToListen]);
+
+  return { sendMessage, receivedData };
+};
+
+export default useWebSocket;
